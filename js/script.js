c939c2778ec7beeba3461a4d4aad8f0ec234367d
yanny ouzid <yanny.ouzid@edu.devinci.fr>
2019-11-18 17:50:07 +0100

    ajout des sons

--- js/script.js
+++ js/script.js
@@ -24,3 +24,33 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+
+
+function audioDo() {
+
+	var sound = document.getElementById("audiodo");
+	sound.play();
+
+}
+
+function audioRe() {
+
+	var sound = document.getElementById("audiore");
+	sound.play();
+
+}
+
+function audioMi() {
+
+	var sound = document.getElementById("audiomi");
+	sound.play();
+
+}
+
+function audioFa() {
+
+	var sound = document.getElementById("audiofa");
+	sound.play();
+
+}
