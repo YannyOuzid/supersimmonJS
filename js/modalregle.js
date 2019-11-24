5bc67adec34153805b5a69b984e78951fc08725c
yanny ouzid <yanny.ouzid@edu.devinci.fr>
2019-11-24 15:25:05 +0100

    ajout bouton recommencer

--- js/modalregle.js
+++ js/modalregle.js
@@ -27,30 +27,3 @@ window.onclick = function(event) {
 
 
 
-function audioDo() {
-
-	var sound = document.getElementById("audiodo");
-	sound.play();
-
-}
-
-function audioRe() {
-
-	var sound = document.getElementById("audiore");
-	sound.play();
-
-}
-
-function audioMi() {
-
-	var sound = document.getElementById("audiomi");
-	sound.play();
-
-}
-
-function audioFa() {
-
-	var sound = document.getElementById("audiofa");
-	sound.play();
-
-}
---
+++ js/son.js
@@ -0,0 +1,27 @@
+function audioDo() {
+
+	var sound = document.getElementById("audiodo");
+	sound.play();
+
+}
+
+function audioRe() {
+
+	var sound = document.getElementById("audiore");
+	sound.play();
+
+}
+
+function audioMi() {
+
+	var sound = document.getElementById("audiomi");
+	sound.play();
+
+}
+
+function audioFa() {
+
+	var sound = document.getElementById("audiofa");
+	sound.play();
+
+}
